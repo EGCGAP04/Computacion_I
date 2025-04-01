59a411410631a7200be49230297b33c6e4a24893
Enzo Agustín Aguirre Polenta <ea.aguirrepolenta@gmail.com>
2025-04-01 01:12:37 -0300

    Add files via upload

--- Numeros_Romanos.py
+++ Numeros_Romanos.py
@@ -9,8 +9,9 @@ print("Escriba 'Fin' para finalizar el programa.")
 
 entrada = ""
 caracteres = []
-valores_romanos = {"i": 1, "v": 5, "x": 10, "l": 50, "c": 100, "d": 500, "m": 1000, " ": 0}
+valores_romanos = {"i": 1, "v": 5, "x": 10, "l": 50, "c": 100, "d": 500, "m": 1000}
 permitidos_restar = ["i", "x", "c"]
+no_repetibles = ["v", "l", "d"]
 
 while entrada not in ["fin", "'fin'", "'fin", "fin'"]:
     caracteres.clear()
@@ -19,7 +20,7 @@ while entrada not in ["fin", "'fin'", "'fin", "fin'"]:
     entrada = input("Ingresa un número romano: ").strip().lower()
 
     if entrada not in ["fin", "'fin'", "'fin", "fin'"]:
-        caracteres = list(entrada)
+        caracteres = [i for i in entrada if i != " "]
 
         for i in caracteres:
             if i not in valores_romanos:
@@ -32,17 +33,30 @@ while entrada not in ["fin", "'fin'", "'fin", "fin'"]:
                 j1 = caracteres[i + 1]
 
                 if valores_romanos[j0] < valores_romanos[j1]:
-                    if j0 not in permitidos_restar:
+                    if j0 not in permitidos_restar or j1 in no_repetibles:
                         caracter_invalido = 2
                         break
 
         if caracter_invalido == 0:
-            caracteres = [valores_romanos[c] for c in caracteres]
+            j = 1
+            for i in range(1, len(caracteres)):
+                if caracteres[i] == caracteres[i - 1]:
+                    j += 1
+                    if caracteres[i] in no_repetibles or j > 3:
+                        caracter_invalido = 3
+                        break
+                else:
+                    j = 1
+
+        if caracter_invalido == 0:
+            caracteres = [valores_romanos[i] for i in caracteres]
 
         if caracter_invalido == 1:
             print("No ha ingresado un número romano válido.")
         elif caracter_invalido == 2:
-            print("Error: Solo I, X y C pueden restarse.")
+            print("Solo I, X y C pueden restarse.")
+        elif caracter_invalido == 3:
+            print("I, X, C y M solo se pueden repetir hasta tres veces y V, L y D no pueden repetirse.")
         else:
             print(caracteres)
 
